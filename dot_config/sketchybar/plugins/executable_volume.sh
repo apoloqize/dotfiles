5622c631676fb6c777664e58c9173d54fcf00162
apoloqize <apoloqize@users.noreply.github.com>
2024-10-27 15:52:46 +0100

    fix volume

--- dot_config/sketchybar/plugins/executable_volume.sh
+++ dot_config/sketchybar/plugins/executable_volume.sh
@@ -2,11 +2,6 @@
 
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
-MODEL=$(system_profiler SPHardwareDataType | grep "Model Name:" | awk '{print $4}')
-
-if [ "$MODEL" = "mini" ]; then
-    exit 0
-fi
 
 if [ "$SENDER" = "volume_change" ]; then
   VOLUME="$INFO"
