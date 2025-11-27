1c3b33fbdb368274cd08e7b8f7aba4bcde4cb9b3
apoloqize <apoloqize@users.noreply.github.com>
2025-11-27 21:38:25 +0100

    reinit nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,2 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.g.have_nerd_font = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.cmdheight = 0
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
-})
-
--- Keybindings
-vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Toggle Neo Tree' })
-
 require("config.lazy")
+
