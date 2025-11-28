5c8f374edc235ff20582b15d675309fb87e65149
apoloqize <apoloqize@users.noreply.github.com>
2025-11-28 14:51:11 +0100

    add plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,17 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 10
+
 require("config.lazy")
 
+-- Keybindings
+-- Toggle Oil file explorer
+local function toggle_oil()
+  if vim.bo.filetype == "oil" then
+    vim.cmd("bd")  -- Close Oil buffer
+  else
+    vim.cmd("Oil")  -- Open Oil
+  end
+end
+
+vim.keymap.set("n", "<leader>e", toggle_oil, { desc = "Toggle file explorer" })
---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,5 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-mini/mini.icons' },
+    opts = {}
+}
